1712f25bc2a8b7f3b31d4c1d5007d35adfff7cde
HiMatterhorn <wyp4tomasz@gmail.com>
2022-05-03 10:09:21 +0200

    CarModel class created

---
+++ Models/CarModel.cs
@@ -0,0 +1,19 @@
+﻿using static AmiFlota.Utilities.Enums;
+
+namespace AmiFlota.Models
+{
+    public class CarModel
+    {
+
+        public string VINnumber { get; set; }
+        public string RegistrationNumber { get; set; }
+
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        public int SeatsNumber { get; set; }
+
+        public TrunkType Trunk { get; set; }
+
+    }
+    }
---
+++ Utilities/Enums.cs
@@ -0,0 +1,10 @@
+﻿namespace AmiFlota.Utilities
+{
+    public class Enums
+    {
+        public enum TrunkType
+        {
+            hatchback, sedan, combi, van, suv
+        }
+    }
+}
